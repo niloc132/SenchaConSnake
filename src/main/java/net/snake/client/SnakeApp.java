39fa884c2e35f11ded9bdf24ecbaa4db4182ee6f
Colin Alworth <niloc132@gmail.com>
2011-10-26 10:28:33 -0500

    Creating the basic struture, simple User and simple direction events. Lots of TODOs.

--- src/main/java/net/snake/client/SnakeApp.java
+++ src/main/java/net/snake/client/SnakeApp.java
@@ -1,13 +1,25 @@
 package net.snake.client;
 
+import net.snake.client.widget.SideBar;
+
 import com.google.gwt.core.client.EntryPoint;
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.event.shared.SimpleEventBus;
+import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer;
+import com.sencha.gxt.widget.core.client.container.BorderLayoutContainer.BorderLayoutData;
 
 public class SnakeApp implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		// TODO Auto-generated method stub
-
+		BorderLayoutContainer root = new BorderLayoutContainer();
+		
+		EventBus bus = new SimpleEventBus();
+		//TODO add event preview for arrows, send events on bus
+		
+		SideBar east = new SideBar(bus);
+		BorderLayoutData eastData = new BorderLayoutData(300);
+		root.setEastWidget(east, eastData);
 	}
 
 }
