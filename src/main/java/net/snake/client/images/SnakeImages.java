ac02cb186233ac7528c5898cb3fb0d7d3c6341b0
Daniel Ardison <dardison@gmail.com>
2011-10-26 12:59:24 -0500

    Arrow Buttons

---
+++ src/main/java/net/snake/client/images/SnakeImages.java
@@ -0,0 +1,24 @@
+package net.snake.client.images;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.resources.client.ImageResource;
+
+public interface SnakeImages extends ClientBundle {
+	
+	SnakeCssResource resources();
+	
+	@Source("UpArrow.png")
+	ImageResource north();
+	
+	@Source("DownArrow.png")
+	ImageResource south();
+	
+	@Source("LeftArrow.png")
+	ImageResource west();
+	
+	@Source("RightArrow.png")
+	ImageResource east();
+	
+
+}
--- src/main/java/net/snake/client/widget/SideBar.java
+++ src/main/java/net/snake/client/widget/SideBar.java
@@ -1,21 +1,36 @@
 package net.snake.client.widget;
 
+import net.snake.client.event.DirectionCommandEvent;
+import net.snake.client.event.DirectionCommandEvent.Direction;
+import net.snake.client.images.SnakeImages;
 import net.snake.shared.User;
 
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.web.bindery.event.shared.EventBus;
 import com.sencha.gxt.core.client.IdentityValueProvider;
 import com.sencha.gxt.widget.core.client.ListView;
+import com.sencha.gxt.widget.core.client.button.IconButton;
+import com.sencha.gxt.widget.core.client.button.TextButton;
 import com.sencha.gxt.widget.core.client.container.FlowLayoutContainer;
+import com.sencha.gxt.widget.core.client.container.HtmlLayoutContainer;
 
 public class SideBar implements IsWidget {
 	private FlowLayoutContainer sideBar;
 	private ListView<User, User> userList;
+	private IconButton north;
+	private IconButton east;
+	private IconButton south;
+	private IconButton west;
+	private EventBus bus;
 
 	public SideBar(EventBus bus) {
 		//TODO watch for events
-		
+		this.bus=bus;
 		
 		sideBar = new FlowLayoutContainer();
 		
@@ -32,6 +47,63 @@ public class SideBar implements IsWidget {
 	@Override
 	public Widget asWidget() {
 		return sideBar;
+	}
+	
+	protected void buildButtonsPanel(){
+		SnakeImages images=GWT.create(SnakeImages.class);
+		
+		this.north=new IconButton(images.resources().up());
+		this.north.setWidth(32);
+		this.north.setHeight(32);
+		this.north.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				DirectionCommandEvent northEvent=new DirectionCommandEvent(Direction.NORTH);
+				bus.fireEventFromSource(northEvent, this);
+			}
+		});
+		
+		this.south=new IconButton(images.resources().down());
+		this.south.setWidth(32);
+		this.south.setHeight(32);
+		this.south.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				DirectionCommandEvent southEvent=new DirectionCommandEvent(Direction.SOUTH);
+				bus.fireEventFromSource(southEvent, this);
+				
+			}
+		});
+		
+		this.east=new IconButton(images.resources().right());
+		this.east.setWidth(32);
+		this.east.setHeight(32);
+
+		this.east.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				DirectionCommandEvent eastEvent=new DirectionCommandEvent(Direction.EAST);
+				bus.fireEventFromSource(eastEvent, this);
+				
+			}
+		});
+		
+		this.west=new IconButton(images.resources().left());
+		this.west.setWidth(32);
+		this.west.setHeight(32);
+
+		this.west.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				DirectionCommandEvent westEvent=new DirectionCommandEvent(Direction.WEST);
+				bus.fireEventFromSource(westEvent, this);
+				
+			}
+		});
 	}
 
 }
