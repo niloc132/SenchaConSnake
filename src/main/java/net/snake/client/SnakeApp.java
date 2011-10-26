1c9efc98f3ffc3f7e83e3f915d28e35f5f265d2b
Colin Alworth <niloc132@gmail.com>
2011-10-26 09:57:11 -0500

    Initial commit wiht a project that almost compiles, still missing web.xml and an html file

---
+++ src/main/java/net/snake/client/SnakeApp.java
@@ -0,0 +1,13 @@
+package net.snake.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class SnakeApp implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
